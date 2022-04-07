13bd8ab1f11b5c6da4637072cbf8e3d05cf8d4c9
Oleg Kostrytsyn <okostritsyn@gmail.com>
2022-04-08 01:16:36 +0300

    add adapter class

---
+++ src/main/java/adapter/adapters/SquarePegAdapter.java
@@ -0,0 +1,23 @@
+package main.java.adapter.adapters;
+
+import main.java.adapter.round.RoundPeg;
+import main.java.adapter.square.SquarePeg;
+
+/**
+ * Адаптер позволяет использовать КвадратныеКолышки и КруглыеОтверстия вместе.
+ */
+public class SquarePegAdapter extends RoundPeg {
+    private SquarePeg peg;
+
+    public SquarePegAdapter(SquarePeg peg) {
+        this.peg = peg;
+    }
+
+    @Override
+    public double getRadius() {
+        double result;
+        // Рассчитываем минимальный радиус, в который пролезет этот колышек.
+        result = (Math.sqrt(Math.pow((peg.getWidth() / 2), 2) * 2));
+        return result;
+    }
+}
